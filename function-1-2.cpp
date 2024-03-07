c0da2509d989ec91eb642e0052935e9fb608cae1
Minh Chiến <tom@student-10-201-12-069.wireless.adelaide.edu.au>
2024-03-07 11:02:04 +1030

    version2

--- function-1-2.cpp
+++ function-1-2.cpp
@@ -1,12 +1,10 @@
 int is_identity(int array[10][10]) {
-    int sum = 0;
-    int sumDiagonal = 0;
     for (int i = 0; i<10; i++ ){
         for (int j = 0; j<10 ;j++){
-        if ((array[i][i]==1) && (i!=j && array[i][j]==0)){
-            return 1;
+        if ((i == j && array[i][j]!=1) || (i!=j && array[i][j]!=0)){
+            return 0;
         }
  }
     }
-    return 0;
+    return 1;
 }
