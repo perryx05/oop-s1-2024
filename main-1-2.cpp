c0d796f860e2b0bdd0e7cc974f85478e55182064
Minh Chiến <tom@student-10-201-13-147.wireless.adelaide.edu.au>
2024-03-14 10:44:07 +1030

    first version

--- main-1-2.cpp
+++ main-1-2.cpp
@@ -6,7 +6,7 @@ extern void modifyArray(double* array, int size, double value);
 int main(){
     double array[8] = {1.0,2.0,3.5,4.6,5.6,6.7,7.8,8.2};
     int size = 8;
-    double value = 2;
+    double value = 2;   
     modifyArray(array, size, value);
     for (int i = 0; i<8; i++){
         cout<<array[i]<<" ";
