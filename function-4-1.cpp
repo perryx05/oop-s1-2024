0864ce5cb28fe9996c8043cbd5e4019eda319bab
perryx05 <144699055+perryx05@users.noreply.github.com>
2024-03-20 01:14:50 +1030

    Update function-4-1.cpp
    draft but right algorithm
    
    Signed-off-by: perryx05 <144699055+perryx05@users.noreply.github.com>

--- function-4-1.cpp
+++ function-4-1.cpp
@@ -1,26 +1,43 @@
-# include <iostream>
+
+// Online IDE - Code Editor, Compiler, Interpreter
+
+// Online C++ compiler to run C++ program online
+#include <iostream>
 using namespace std;
 
-int *readNumbers (){
-    int length = 10;
-    int *array = new int[10];
-    for (int i = 0;i<length; i++){
-        cin>>array[i];
+int *secondSmallestSum(int *array,int length) {
+    int sum = (1+length) * length/2;
+    int *sumArray = new int [sum];
+    for (int i = 0; i<length; i++){
+        sumArray[i] = array[i];
     }
-   return array;
+    int index = length;
+  
+    for (int i = 0; i<length-1; i++){
+        int num = array[i];
+        for (int j = i+1;j<length;j++){
+            num = num+array[j];
+            sumArray[index] = num;
+            index++;
+            
+        }
+        num = 0;
+    }
+    
+
+    return sumArray;
 }
 
-int secondSmallestSum(int *array,int length) {
-    int num;
-   for (int i = 0; i<length; i++){
-        for (int j = i+1; j<length; j++){
-           if (array[i]>=array[j]){
-           num = array[i];
-           array[i] = array[j];
-           array[j] = num;
-           }
+    int main(){
+       int *array = new int[10];
+        for (int i = 0; i<5; i++){
+            array[i]=i+1;
         }
-        
+        int *array2 = secondSmallestSum(array, 5);
+       for (int i =0; i<15; i++){
+           cout<<array2[i]<<" ";
+       }
     }
-    return array[0] + array[1];
-}
+    
+    
+    
