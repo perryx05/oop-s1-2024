06b6409da438973838de0273c3a174bc02f9c37a
perryx05 <minhchien0305@gmail.com>
2024-03-28 11:51:01 +1030

    .

--- Unit.cpp
+++ Unit.cpp
@@ -6,20 +6,25 @@ Unit :: Unit(){
     this -> unit_val = 0;
     this -> num_beds = 0;
     this -> unit_size = 0;
-     if (unit_val<0 ){
-         this -> unit_val = 0;
-    }
-    if (num_beds <0){
-        this -> num_beds = 0;
-    }
-    if (unit_size <0){
-        this -> unit_size = 0;
-    }
+    
 }
 Unit :: Unit(int unit_val, int num_beds, double unit_size){
-    this -> unit_val = unit_val;
-    this -> num_beds = num_beds;
-    this -> unit_size = unit_size;
+   if (unit_val<0 ){
+        this -> unit_val = 0;
+    } else {
+        this -> unit_val = unit_val;
+    }
+
+    if (num_beds <0){
+        this -> num_beds = 0;
+    } else {
+        this -> num_beds = num_beds;
+    }
+    if (unit_size <0){
+       this -> unit_size = 0;
+    } else {
+        this -> unit_size = unit_size;
+    }
 }
 int Unit :: get_Num_Bedrooms(){
     return num_beds;
--- main-1-1.cpp
+++ main-1-1.cpp
@@ -7,4 +7,6 @@ int main (){
     cout<<" "<<unit1.get_Value()<<" "<<unit1.get_Num_Bedrooms()<<" "<<unit1.get_Area()<<endl;
    Unit unit2(20,3,50);
    cout<<" "<<unit2.get_Value()<<" "<<unit2.get_Num_Bedrooms()<<" "<<unit2.get_Area()<<endl;
+   Unit unit3(-10,-5,-6);
+   cout<<" "<<unit3.get_Value()<<" "<<unit3.get_Num_Bedrooms()<<" "<<unit3.get_Area()<<endl;
 }
