370d4043a20c86cfc4b557973a6f77996cd8f88e
perryx05 <minhchien0305@gmail.com>
2024-03-28 11:44:57 +1030

    .

--- Unit.cpp
+++ Unit.cpp
@@ -6,20 +6,20 @@ Unit :: Unit(){
     this -> unit_val = 0;
     this -> num_beds = 0;
     this -> unit_size = 0;
+     if (unit_val<0 ){
+         this -> unit_val = 0;
+    }
+    if (num_beds <0){
+        this -> num_beds = 0;
+    }
+    if (unit_size <0){
+        this -> unit_size = 0;
+    }
 }
 Unit :: Unit(int unit_val, int num_beds, double unit_size){
     this -> unit_val = unit_val;
     this -> num_beds = num_beds;
     this -> unit_size = unit_size;
-    if (unit_val<0 ){
-         unit_val = 0;
-    }
-    if (num_beds <0){
-        num_beds = 0;
-    }
-    if (unit_size <0){
-        unit_size = 0;
-    }
 }
 int Unit :: get_Num_Bedrooms(){
     return num_beds;
