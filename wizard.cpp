914c7ff19a0b9514f00f94d4a22c9bcfe9546375
perryx05 <minhchien0305@gmail.com>
2024-04-02 10:38:42 +1030

    first ver

--- wizard.cpp
+++ wizard.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 Wizard :: Wizard(string name, int health, int damage, int mana) : Player(name, health, damage), mana(mana){}
-void Wizard::castSpell(Player *opponent){
+void Wizard:: castSpell(Player *opponent){
      opponent -> takeDamage (mana);
      cout << name << " casts a spell on " << opponent->getName() << " for " << damage << " damage.\n";
 }
