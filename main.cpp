89a8ecf3296e15dee0d29b06f4db9fac6342cb02
perryx05 <minhchien0305@gmail.com>
2024-05-06 15:06:32 +0930

    second ver

--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
 #include "UnitTest.h"
 #include "EquivalenceTest.h"
+#include "InvoiceTest.h"
 
 int main() {
     UnitTest unitTest;
     unitTest.runTests();
+
     EquivalenceTests equivalenceTests;
     equivalenceTests.runTests();
+
+    InvoiceTest invoiceTest;
+    invoiceTest.runTests();
     return 0;
 }
