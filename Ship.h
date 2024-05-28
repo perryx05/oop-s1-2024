8e279dd7ffec864c280fead984b7d21de744a80b
perryx05 <minhchien0305@gmail.com>
2024-05-28 10:40:43 +0930

    fixed

--- Ship.h
+++ Ship.h
@@ -7,10 +7,7 @@ class Ship : public GameEntity{
 public:
 Ship (int x, int y ) : GameEntity(x, y, 'S'){}
 void move (int dx, int dy){
-    std::tuple<int, int> pos = getPos();
-    int x = std::get<0>(pos)+dx;
-    int y = std::get<1>(pos)+dy;
-    pos = std::make_tuple(x,y);
+   this -> position = make_tuple(dx,dy);
 }
 };
 #endif
